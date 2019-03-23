fdff3ea398c5e62939f57551b350ac35f8ed3766
Mourad <mourad.eyes@gmail.com>
2019-03-23 20:37:22 +0100

    created and app for making hash function just click on the html

---
+++ crypto_js/crypto.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("get_hash");
+
+button.addEventListener("click", function() {
+  const text = document.getElementById("text").value;
+  const output = document.getElementById("output");
+  const title = document.getElementById("title");
+  if (text == "") {
+    title.innerHTML = "Please inter a text to hash";
+    output.innerHTML = "";
+  } else {
+    title.innerHTML = "your hash is:";
+    const hash = CryptoJS.SHA256(text);
+    output.innerHTML = hash;
+  }
+});
